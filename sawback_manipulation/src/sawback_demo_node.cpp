f246df35d0506c07e52df1b563c2c904e3932920
bostoncleek <bocl8874@colorado.edu>
2020-10-08 16:16:36 -0500

    move to starting configuration

--- sawback_manipulation/src/sawback_demo_node.cpp
+++ sawback_manipulation/src/sawback_demo_node.cpp
@@ -63,11 +63,49 @@ int main(int argc, char** argv)
   auto moveit_cpp_ptr = std::make_shared<moveit::planning_interface::MoveItCpp>(nh);
   moveit_cpp_ptr->getPlanningSceneMonitor()->providePlanningSceneService();
 
+  moveit::planning_interface::PlanningComponentPtr planning_component_ptr =
+      std::make_shared<moveit::planning_interface::PlanningComponent>("right_arm", moveit_cpp_ptr);
+
+  // TODO: add floor and object
   // const moveit_msgs::CollisionObject box = createObject();
 
+  // starting configuration
+  const double initial_joints[] = { 0.9389267787249193,  -0.2050500749605062, -1.0421734917046992, 1.4588063551704888,
+                                    -1.8597856835303392, -1.0772340009395818, -0.22431043800663522 };
+
+  // move to starting configuration
+  const moveit::core::RobotModelConstPtr robot_model =
+      std::const_pointer_cast<const moveit::core::RobotModel>(moveit_cpp_ptr->getRobotModel());
+
+  moveit::core::RobotStatePtr robot_state_ptr = make_shared<moveit::core::RobotState>(robot_model);
+
+  const moveit::core::JointModelGroup* joint_model_group_ptr = robot_state_ptr->getJointModelGroup("right_arm");
+
+  // Set joint angles to positions from IK
+  robot_state_ptr->setJointGroupPositions(joint_model_group_ptr, initial_joints);
+  // Update all the transforms
+  robot_state_ptr->update(initial_joints);
+
+  planning_component_ptr->setStartStateToCurrentState();
+  planning_component_ptr->setGoal(const_cast<const robot_state::RobotState&>(*robot_state_ptr.get()));
+
+  const moveit::planning_interface::PlanningComponent::PlanSolution plan = planning_component_ptr->plan();
+
+  if (plan.error_code == moveit_msgs::MoveItErrorCodes::SUCCESS)
+  {
+    moveit_cpp_ptr->execute("right_arm", plan.trajectory);
+  }
+
+  else
+  {
+    ROS_INFO_STREAM_NAMED(LOGNAME, "Failed to move to initial configuration");
+    ros::shutdown();
+    return 0;
+  }
+
+  // Begin manipulation tasks
   auto task = std::make_unique<sawback_manipulation::tasks::PickPlace>(moveit_cpp_ptr, "right_arm", "hand",
                                                                        "right_gripper_tip");
-
   // const Eigen::Isometry3d goal =
   //     Eigen::Translation3d(0.7, 0.0303, 0.6069) * Eigen::Quaterniond(0.696, 0.123, 0.696, 0.123);
 
