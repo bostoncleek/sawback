f3d9c7fd487581c6b3523d058912b03127ea693e
bostoncleek <bocl8874@colorado.edu>
2020-11-02 16:27:44 -0600

    description of node added

--- sawback_manipulation/src/grasp_detection_node.cpp
+++ sawback_manipulation/src/grasp_detection_node.cpp
@@ -3,6 +3,27 @@
  * @author Boston Cleek
  * @date 9 Oct 2020
  * @brief Grasp detection node using point clouds
+
+  PARAMETERS:
+    remove_ground - bool segment ground plane from point cloud
+    cartesian_limits - bool removed point from cloud
+    filtered_cloud_frame - frame ID of filtered point cloud
+    path_to_gpd_config - path to GPD config file
+    view_point - (x,y,z) view point of camera usually (0,0,0)
+    transfrom_base_camera - transform from robot base to camera link
+    transfrom_camera_optical - transform from camera link to optical link
+    xyz_lower_limits (optional) - lower limits on point cloud if enabled cartesian_limits
+    xyz_upper_limits (optional)-  upper limits on point cloud if enabled cartesian_limits
+
+  PUBLISHES:
+    filtered_cloud (sensor_msgs/PointCloud2) - filtered point cloud
+    gpose (geometry_msgs/PoseStamped) - highest ranked grasp
+
+  SUBSCRIBES:
+    cloud (sensor_msgs/PointCloud2) - point cloud used for grasp detection
+
+  SERVICES:
+    get_grasp (sawback_msgs/SampleGrasps) - calls GPD alogrithm 
  */
 
 #include <grasp_detection.hpp>
