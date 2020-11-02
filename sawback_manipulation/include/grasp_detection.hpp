893103deeaf9c81403c93017bcc064ecd8dbf79d
bostoncleek <bocl8874@colorado.edu>
2020-11-02 16:14:03 -0600

    doxygen style function comments

--- sawback_manipulation/include/grasp_detection.hpp
+++ sawback_manipulation/include/grasp_detection.hpp
@@ -23,31 +23,58 @@
 
 namespace sawback_manipulation
 {
+/** @brief Grasp detection pipeline */
 class GraspDetection
 {
 public:
+  /**
+   * @brief Constructor
+   * @param nh - node handle
+   */
   GraspDetection(const ros::NodeHandle& nh);
 
 private:
+  /** @brief Load parameters */
   void loadParameters();
 
+  /** @brief Initialize node */
   void init();
 
+  /**
+   * @brief Stereo camera point cloud call back
+   * @param msg - point cloud message
+   */
   void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
 
+  /**
+   * @brief Detect grasps service call back
+   * @param res[out] - result containing the highest ranked grasp
+   * @return true if successfully detected grasps
+   */
   bool graspCallback(sawback_msgs::SampleGrasps::Request&, sawback_msgs::SampleGrasps::Response& res);
 
+  /**
+   * @brief Perform point cloud pre-processing before GPD
+   * @param grasp_cloud[out] - processed point cloud
+   * @details Can segment ground plan and restrict point cloud based on cartesian limits
+   */
   void processCloud(PointCloudRGBA::Ptr& grasp_cloud);
 
+  /**
+   * @brief Calls GPD grasp pose detection algorithm
+   * @param grasp[out] - highest ranked grasp
+   * @param grasp_cloud - point cloud used to detect grasps
+   * @return true if grasps are detected
+   */
   bool sampleGrasps(geometry_msgs::PoseStamped& grasp, const PointCloudRGBA::Ptr& grasp_cloud);
 
 private:
-  ros::NodeHandle nh_;
+  ros::NodeHandle nh_;               // node handle
   ros::Subscriber cloud_sub_;        // point cloud subscriber
   ros::Publisher cloud_pub_;         // publishes the point cloud saved
   ros::ServiceServer grasp_server_;  // grasp server
 
-  ros::Publisher pose_pub_;
+  ros::Publisher pose_pub_;  // pub;ish grasp pose
 
   bool remove_ground_;     // specify if to remove table points
   bool cartesian_limits_;  // specify if to remove points outside limits
